6b7855498fe2dc8b3db3d09b13de79123775a77a
AJ Jalusi <ajalusi26@gmail.com>
2021-12-12 16:55:15 -0500

    commit tree dcd7ad70a64925590b2a4201989cc1c360b9b207

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 7.87400787401;
+let num2 = 7.87400787401;
+let multiply = Math.round(num1 * num1);
+console.log(Math.round(multiply))
+
+let random = Math.round(1 + Math.random());
+
+let num3 = 13;
+let num4 = 9;
+let mod = num3 % num4;
+
+
+let max = Math.max(2, 4, 5, 15, 20)
